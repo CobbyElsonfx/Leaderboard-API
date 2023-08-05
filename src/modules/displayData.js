d23c34bfa2645f4cbad3453a989286efae66350e
MR. ANDOH FRANCIS <andohfrancis9187@gmail.com>
2023-08-05 06:07:25 +0000

    assed stylings to the leaderboard

--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -3,11 +3,56 @@ const displayData = async () => {
   const arrData = await data.json();
   const tableContainer = document.querySelector('.tableContainer');
   tableContainer.innerHTML = '';
-  arrData.result.sort((a, b) => b.score - a.score).forEach((element) => {
-    const eachDetail = `
-         <span class="username pl-1">${element.user}</span>:
-         <span class="scores">${element.score}</span>
+  arrData.result.sort((a, b) => b.score - a.score).forEach((element, index) => {
+    const addPositionSuffix = (number) => {
+      if (number > 3) {
+        return 'th';
+      }
+      return '';
+    };
+
+    const medalPosition = [
+      './images/first.png',
+      './images/second.png',
+      './images/third.png',
+    ];
+
+    const position = index + 1;
+    const positionSuffix = addPositionSuffix(position);
+    let eachDetail;
+    if (position < 4) {
+      eachDetail = `
+        <div>
+         <img src="${medalPosition[position - 1]}" alt="medal" class="medal"/>
+        </div>
+        <div><img src="./images/user.png" alt="user"  class="userProfile"></div>
+        <div class="nameAndScore">
+          <div> <h3 class="username pl-1">${element.user.toUpperCase()}</h3></div>
+          <div >
+            <h3 class="scores">${element.score}</h3>
+          </div>
+        </div>
+        </div>
        `;
+    } else {
+      eachDetail = `
+      <div>
+       <span class="position">${position}${positionSuffix}</span>
+      </div>
+      <div>
+      <img src="./images/user.png" alt="user"  class="userProfile">
+      </div>
+    
+      <div class="nameAndScore">
+        <div> <h3 class="username pl-1">${element.user.toUpperCase()}</h3></div>
+        <div>
+          <h3 class="scores">${element.score}</h3>
+        </div>
+     
+      </div>
+      </div>
+     `;
+    }
 
     // Create a new DOM element and set its innerHTML
     const listItem = document.createElement('li');
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)/i,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
       {
